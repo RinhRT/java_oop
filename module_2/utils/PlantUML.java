bcdade2dff7faf4d8746bc7816c8c57cf192fd22
RinhRT <rinhrt5@gmail.com>
2026-08-29 13:01:16 +0700

    Sửa comment trong PlantUML

--- module_2/utils/PlantUML.java
+++ module_2/utils/PlantUML.java
@@ -7,10 +7,10 @@ public class PlantUML {
     }
 
     private String getModifier(int modifiers) {
-        if (Modifier.isPrivate(modifiers)) return "- ";
-        if (Modifier.isProtected(modifiers)) return "# ";
-        if (Modifier.isPublic(modifiers)) return "+ ";
-        return "~ ";
+        if (Modifier.isPrivate(modifiers)) return "- "; // Private
+        if (Modifier.isProtected(modifiers)) return "# "; // Protect
+        if (Modifier.isPublic(modifiers)) return "+ "; // Public
+        return "~ "; // Default
     }
 
     private String formatType(Class<?> type) {
@@ -20,6 +20,7 @@ public class PlantUML {
     public void printUML(Class<?> cl) {
         System.out.println(cl.getSimpleName() + " {");
 
+        // Lấy và in ra các thuộc tính của class
         Field[] fields = cl.getDeclaredFields();
         for (Field field : fields) {
             field.setAccessible(true);
@@ -27,9 +28,12 @@ public class PlantUML {
             System.out.println("  " + visibility + formatType(field.getType()) + " " + field.getName());
         }
 
+        // In ra các hàm khởi tạo trong class
         Constructor<?>[] constructors = cl.getDeclaredConstructors();
         for (Constructor<?> constructor : constructors) {
             String visibility = this.getModifier(constructor.getModifiers());
+
+            // Lấy ra các param hiện có trong hàm khởi tạo và thêm vào mảng params[]
             StringBuilder params = new StringBuilder();
             for (Parameter param : constructor.getParameters()) {
                 if (params.length() > 0) params.append(", ");
@@ -38,9 +42,12 @@ public class PlantUML {
             System.out.println("  " + visibility + cl.getSimpleName() + "(" + params + ")");
         }
 
+        // Lấy và in ra các phương thức hiện có trong class 
         Method[] methods = cl.getDeclaredMethods();
         for (Method method : methods) {
             String visibility = this.getModifier(method.getModifiers());
+
+            // Lấy ra các param hiện có trong hàm và thêm vào mảng params[]
             StringBuilder params = new StringBuilder();
             for (Parameter param : method.getParameters()) {
                 if (params.length() > 0) params.append(", ");
